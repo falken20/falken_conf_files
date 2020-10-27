2f3ce097a6fee8628520edbb24fe7b0308fd34af
Richi Rod <ricardorg20@gmail.com>
2020-10-27 12:53:05 +0100

    Update error.py

--- error.py
+++ error.py
@@ -1,13 +1,18 @@
 # Go to the parent directory
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# Set the format of logging messages
+logging.basicConfig(format=f'{os.getenv("ID_LOG", "")} %(levelname)s:%(asctime)s: '
+                           f'File: %(filename)s: Function: %(funcName)s\n'
+                           '%(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=os.getenv("LOG_LEVEL", "INFO"))
+
 try:
   pass
   
 except Exception as err:
-    logging.error(f'{os.getenv("ID_LOG", "")} Error in method create_list_urls \n'
-                  f'Line: {err.__traceback__.tb_lineno} \n'
-                  f'File: {err.__traceback__.tb_frame.f_code.co_filename} \n'
+    logging.error(f'Line: {err.__traceback__.tb_lineno} \n'
                   f'Type: {type(err).__name__} \n'
                   f'Arguments:\n {err.args}')
     raise
