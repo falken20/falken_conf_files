94528c1ec3c3d7aa9ed93995e6abc3f77628d427
Richi Rod <ricardorg20@gmail.com>
2020-10-27 12:17:55 +0100

    Update error.py

--- error.py
+++ error.py
@@ -8,6 +8,6 @@ except Exception as err:
     logging.error(f'{os.getenv("ID_LOG", "")} Error in method create_list_urls \n'
                   f'Line: {err.__traceback__.tb_lineno} \n'
                   f'File: {err.__traceback__.tb_frame.f_code.co_filename} \n'
-                  f'Type Error: {type(err).__name__} \n'
+                  f'Type: {type(err).__name__} \n'
                   f'Arguments:\n {err.args}')
     raise
