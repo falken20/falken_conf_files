2174a88e11c15cea6be76db14dc4f81cbe009cb3
Richi Rod <ricardorg20@gmail.com>
2021-02-11 22:35:36 +0100

    Update config_fk.py

--- config_fk.py
+++ config_fk.py
@@ -1,39 +1,52 @@
 # by Richi Rod AKA @richionline / falken20
 
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Load .env file
+load_dotenv(find_dotenv())
+
 """
 # Access to the file:
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config_fk import SETUP_DATA
-
-# Load env file
-load_dotenv(find_dotenv())
 """
 
-__title__ = 'Richi Rod Portfolio'
+__title__ = 'Falken Chat'
 __version__ = '1.0.0'
 __author__ = 'Falken'
 __url_github__ = 'https://github.com/falken20/'
 __url_twitter__ = 'https://twitter.com/richionline'
 __url_linkedin__ = 'https://www.linkedin.com/in/richionline/'
 __license__ = 'MIT License'
-__copyright__ = '© 2020 by Richi Rod AKA @richionline / falken20'
+__copyright__ = '© 2021 by Richi Rod AKA @richionline / falken20'
 __features__ = [
 ]
 
 
-SETUP_DATA = {
-    'title': __title__,
-    'version': __version__,
-    'author': __author__,
-    'url_github': __url_github__,
-    'url_twitter': __url_twitter__,
-    'url_linkedin': __url_linkedin__,
-    'license': __license__,
-    'copyrigth': __copyright__,
-    'features': __features__,
-}
+class Config:
+    # Load envioronemnt variables in .env
+    TESTING = os.getenv('TESTING')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SERVER = os.getenv('SERVER')
+    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    ENV_PRO = os.getenv('ENV_PRO')
 
+    # Load setup data
+    SETUP_DATA = {
+        'title': __title__,
+        'version': __version__,
+        'author': __author__,
+        'url_github': __url_github__,
+        'url_twitter': __url_twitter__,
+        'url_linkedin': __url_linkedin__,
+        'license': __license__,
+        'copyrigth': __copyright__,
+        'features': __features__,
+    }
+    
 # .env file
 """
 SECRET_KEY=
